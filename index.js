e65652ffb677a617e8669a2aac28d7370c05a3ed
gylim0604 <gylim0604@hotmail.com>
2022-01-30 20:38:35 +0800

    prepare for push to heroku :q

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const res = require('express/lib/response');
 const morgan = require('morgan');
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
 app.use(express.json());
 morgan.token('body', (req) => {
     return req.method === 'POST' ? JSON.stringify(req.body) : '';
@@ -100,6 +103,6 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 app.listen(PORT);
 console.log(`Server running on port${PORT}`);
